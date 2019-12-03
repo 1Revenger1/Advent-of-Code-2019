d825099e11d3349ea909b59bd9c82553555fb7d0
Avery Black <avery.c.black@gmail.com>
2019-12-02 19:43:21 -0800

    IntCodeinnator...that doesn't sound good for the tristate area

--- Solutions/Template/src/Index.ts
+++ Solutions/Template/src/Index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as chalk from 'chalk';
+import { IntCodeinnator, Storage } from './IntCodeinnator';
 
 const day : number = 0;
 
@@ -8,16 +9,18 @@ console.log("|  " + chalk.blueBright("Advent of Code 2019:") + chalk.green(" Day
 console.log("+------------------------------+\n");
 
 // Get file and split it into lines
-let file : string[] = fs.readFileSync("./input.txt", {
+let file : string = fs.readFileSync("./input.txt", {
     encoding: "utf8",
-}).split("\n");
+});
+
+let storage = Storage.importFile(file);
+let innator = new IntCodeinnator(storage);
 
 // Timing start
 let startTime : number = new Date().getTime();
 
-file.forEach((line : string) => {
-    console.log(line);
-});
+let output = innator.run(12, 2);
+console.log(output[0]);
 
 // Timing end
 let part1End : number = new Date().getTime();
@@ -26,12 +29,13 @@ let part1End : number = new Date().getTime();
 console.log(chalk.yellow("Part 1 done in: ") + chalk.red(part1End - startTime + " ms")
     + chalk.green(" Executing Part 2..."));
 
+innator.reset();
+
 // Timing of Part 2 start
 let startPart2Time : number = new Date().getTime();
 
-file.forEach((line : string) => {
-    console.log(chalk.blue(line));
-});
+output = innator.run(12, 2);
+console.log(output[0]);
 
 // Timing end
 let part2End : number = new Date().getTime();
